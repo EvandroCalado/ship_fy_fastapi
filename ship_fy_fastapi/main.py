b7a1be26fd22aad7d2347b1f3c11035ee43c5b8a
Evandro Calado <evandrocalado07@gmail.com>
2025-05-30 17:20:15 -0300

    feat: Add shipment creation, update, and deletion endpoints

--- ship_fy_fastapi/main.py
+++ ship_fy_fastapi/main.py
@@ -22,18 +22,68 @@ def get_scalar_docs():
     )
 
 
-@app.get('/shipment/latest')
+@app.post('/shipment', status_code=status.HTTP_201_CREATED)
+def create_shipment(data: dict[str, Any]) -> dict[str, Any]:
+    max_weight = 25
+
+    content = data['content']
+    weight = data['weight']
+
+    if weight > max_weight:
+        raise HTTPException(
+            status_code=status.HTTP_406_NOT_ACCEPTABLE,
+            detail='maximum weight limit is 25kgs',
+        )
+
+    new_id = max(shipments.keys()) + 1
+
+    shipments[new_id] = {
+        'content': content,
+        'weight': weight,
+        'status': 'placed',
+    }
+
+    return {'id': new_id}
+
+
+@app.get('/shipment/latest', status_code=status.HTTP_200_OK)
 def get_latest_shipment() -> dict[str, Any]:
     id = max(shipments.keys())
     return shipments[id]
 
 
-@app.get('/shipment/{id}')
+@app.get('/shipment/{id}', status_code=status.HTTP_200_OK)
 def get_shipment(id: int) -> dict[str, Any]:
     if id not in shipments:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail='Shipment not found',
         )
+
+    return shipments[id]
+
+
+@app.patch('/shipment/{id}', status_code=status.HTTP_200_OK)
+def update_shipment(id: int, data: dict[str, Any]) -> dict[str, Any]:
+    if id not in shipments:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Shipment not found',
+        )
+
+    shipments[id].update(data)
+
+    return shipments[id]
+
+
+@app.delete('/shipment/{id}', status_code=status.HTTP_200_OK)
+def delete_shipment(id: int) -> dict[str, Any]:
+    if id not in shipments:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Shipment not found',
+        )
+
+    del shipments[id]
 
     return shipments[id]
